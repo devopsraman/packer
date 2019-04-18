9b1fc7638bccc622f33986a04c48708737cf8b3d
Ram Thalla <devopsraman@gmail.com>
2019-04-18 11:51:19 +0100

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,18 +5,18 @@ sudo  wget https://raw.github.com/devopsraman/packer/master/nginx.repo -P /etc/y
 sudo yum install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
-sudo systemctl start nginx
+#sudo systemctl start nginx
 #sudo firewall-cmd --permanent --zone=public --add-service=http
 #sudo firewall-cmd --permanent --zone=public --add-service=https
 #sudo firewall-cmd --reload
 #configure SSL
-sudo mkdir /etc/nginx/ssl
+#sudo mkdir /etc/nginx/ssl
 #sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
-sudo openssl req -x509  -new \
+#sudo openssl req -x509  -new \
                 -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/nginx.key \
                         -out /etc/nginx/ssl/nginx.crt \
                                 -subj "/C=uk/ST=Utah/L=london/O=devops/OU=IT/CN=nginx"
-sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.old
-sudo  wget https://raw.github.com/devopsraman/packer/master/default.conf -P /etc/nginx/conf.d
-sudo nginx -t
-sudo systemctl restart  nginx
+#sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.old
+#sudo  wget https://raw.github.com/devopsraman/packer/master/default.conf -P /etc/nginx/conf.d
+#sudo nginx -t
+#sudo systemctl restart  nginx
