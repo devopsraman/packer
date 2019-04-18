f5216af72e4c1c15eef3f24fb3bb0106a8ecce5b
Ram Thalla <devopsraman@gmail.com>
2019-04-18 12:34:51 +0100

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -13,9 +13,9 @@ sudo systemctl start nginx
 #sudo mkdir /etc/nginx/ssl
 #sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 #sudo openssl req -x509  -new \
-                -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/nginx.key \
-                        -out /etc/nginx/ssl/nginx.crt \
-                                -subj "/C=uk/ST=Utah/L=london/O=devops/OU=IT/CN=nginx"
+               # -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/nginx.key \
+                    #    -out /etc/nginx/ssl/nginx.crt \
+                           #     -subj "/C=uk/ST=Utah/L=london/O=devops/OU=IT/CN=nginx"
 #sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.old
 #sudo  wget https://raw.github.com/devopsraman/packer/master/default.conf -P /etc/nginx/conf.d
 #sudo nginx -t
